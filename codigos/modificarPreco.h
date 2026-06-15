6c8c067d37de1679ba73766a08ff79958a0a42a8
Rafael <rafaeltiosso@usp.br>
2026-06-15 11:56:56 -0300

    Create modificarPreco.h

---
+++ codigos/modificarPreco.h
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "produtos.h"
+
+void modificarPreco(Produtos **produto);
+
+void modificarPreco(Produtos **produto){
+  int codigo;
+  double precoNovo;
+  scanf(" %d %lf", &codigo, &precoNovo);
+  (*produto)[codigo].preco=precoNovo;
+}
