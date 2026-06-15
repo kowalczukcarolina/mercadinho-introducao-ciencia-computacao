ef57fb4bf8718b17fcbe9b5361becc267a34fdb4
Carolina Kowalczuk <kowalczukcarolina@gmail.com>
2026-06-14 22:20:59 -0300

    ATV#1 - comentários

--- codigos/inserirProduto.h
+++ codigos/inserirProduto.h
@@ -8,10 +8,10 @@ void inserirProduto(int *ponteiroNumeroProdutos, Produtos **produto){
     //preciso passar como ponteiro de ponteiro para o verdadeiro valor de produto seja alterado
     int tamanhoVetorProdutos = 0;
         (*ponteiroNumeroProdutos)++;//++ tem prioridade sobre ponteiro
-        tamanhoVetorProdutos = *ponteiroNumeroProdutos*sizeof(Produtos);
+        tamanhoVetorProdutos = *ponteiroNumeroProdutos*sizeof(Produtos); //multiplicando quant bits da struct pela quant de bits do vetor
 
 
-        *produto = realloc((*produto), tamanhoVetorProdutos);
+        *produto = realloc((*produto), tamanhoVetorProdutos);//realocando o novo tamanho
         scanf(" %s", (*produto)[(*ponteiroNumeroProdutos-1)].nome);
         scanf(" %d", &(*produto)[(*ponteiroNumeroProdutos-1)].quantidade);
         scanf(" %lf", &(*produto)[(*ponteiroNumeroProdutos-1)].preco);
