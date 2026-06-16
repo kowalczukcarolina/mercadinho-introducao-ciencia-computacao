21689d7e2f6271c6aefb6517ddbb54df29caca7c
VictorAdao <victoradao32@gmail.com>
2026-06-16 12:43:12 -0300

    MAIN - colocando os ifs na MAIN

--- codigos/main.c
+++ codigos/main.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include "inserirProduto.h"
+#include "inserirProduto.c"
 
 int main(void){
     int numeroProdutos = 0;
@@ -17,12 +17,29 @@ int main(void){
     scanf("%s", comando);
 
     if(comando[0] == 'I' && comando[1] == 'P'){
+        //INSERIR PRODUTO
         inserirProduto(ponteiroNumeroProdutos, &produto);
         /*printf("--- PRODUTO ADICIONADO ---\n");
         printf("Nome:       %s\n", produto[numeroProdutos - 1].nome);
         printf("Quantidade: %d\n", produto[numeroProdutos - 1].quantidade);
         printf("Preco:      R$ %lf\n", produto[numeroProdutos - 1].preco);
         free(produto);*/
+    }else if(comando[0] == 'A' && comando[1] == 'E'){
+        //AUMENTAR ESTOQUE
+
+    }else if(comando[0] == 'M' && comando[1] == 'P'){
+        //MODIFICAR PREÇO
+        
+    }else if(comando[0] == 'C' && comando[1] == 'E'){
+        //CONSULATAR ESTOQUE
+        
+    }else if(comando[0] == 'C' && comando[1] == 'S'){
+        //CONSULTAR SALDO
+
+    }else if(comando[0] == 'F' && comando[1] == 'E'){
+        //FINALIZAR O DIA
+        break;
+        
     }
 
     }
