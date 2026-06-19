405cef9521c84b0a0f58e2f39fac6e4e760b6089
Carolina Kowalczuk <kowalczukcarolina@gmail.com>
2026-06-18 23:14:50 -0300

    Update main.c

--- codigos/main.c
+++ codigos/main.c
@@ -1,56 +1,47 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include "inserirProduto.c"
-#include "consultarEstoque.c"
-#include "modificarPreco.c"
-#include "realizarVenda.c"
-#include "consultarSaldo.c"
+#include "produtos.h"
 
 int main(void){
     int numeroProdutos = 0;
     int *ponteiroNumeroProdutos = NULL;
     Produtos *produto = NULL;
+    double saldoCaixa = 100.0;
     char comando[3] = {0};
-    double saldo = 100;
-    double *ponteiroSaldo = NULL;
 
     ponteiroNumeroProdutos = &numeroProdutos;
-    ponteiroSaldo = &saldo;
 
     while(1){ //depois mexer no while para ter como sair da execução
-    scanf("%s", comando);
+        scanf("%s", comando); //lendo string comando - dado por 2 letras
 
-    if(comando[0] == 'I' && comando[1] == 'P'){
-        //INSERIR PRODUTO
-        inserirProduto(ponteiroNumeroProdutos, &produto);
-        /*printf("--- PRODUTO ADICIONADO ---\n");
-        printf("Nome:       %s\n", produto[numeroProdutos - 1].nome);
-        printf("Quantidade: %d\n", produto[numeroProdutos - 1].quantidade);
-        printf("Preco:      R$ %lf\n", produto[numeroProdutos - 1].preco);
-        free(produto);*/
-    }else if(comando[0] == 'A' && comando[1] == 'E'){
-        //AUMENTAR ESTOQUE
-
-    }else if(comando[0] == 'M' && comando[1] == 'P'){
-        modificarPreco(&produto);
-    }else if(comando[0] == 'V' && comando[1] == 'E'){ 
-        realizarVenda(&produto, ponteiroSaldo);
-        //REALIZAR VENDA 
-
-    }else if(comando[0] == 'C' && comando[1] == 'E'){
-        consultarEstoque(&produto, ponteiroNumeroProdutos);
-
-        
-    }else if(comando[0] == 'C' && comando[1] == 'S'){
-        //CONSULTAR SALDO
-        consultarSaldo(ponteiroSaldo);
-
-    }else if(comando[0] == 'F' && comando[1] == 'E'){
-        //FINALIZAR O DIA
-        break;
-        
-    }
-
+        if(comando[0] == 'I' && comando[1] == 'P'){
+            inserirProduto(ponteiroNumeroProdutos, &produto);
+            /*printf("--- PRODUTO ADICIONADO ---\n");
+            printf("Nome:       %s\n", produto[numeroProdutos - 1].nome);
+            printf("Quantidade: %d\n", produto[numeroProdutos - 1].quantidade);
+            printf("Preco:      R$ %lf\n", produto[numeroProdutos - 1].preco);
+            free(produto);*/
+        }
+        else if(comando[0]=='A' && comando[1] == 'E'){
+            //a função aumentarEstoque precisa do ponteiro simples (Produtos *) e o endereço do saldo do caixa
+            //a variável produto já é um ponteiro simples (Produtos *produto), então passamos ela diretamente
+            aumentarEstoque(produto, &saldoCaixa); 
+        }
+        else if(comando[0]=='M' && comando[1]=='P'){
+            modificarPreco(&produto);
+        }
+        else if(comando[0]=='V' && comando[1]=='E'){
+            realizarVenda(&produto, &saldoCaixa);
+        }
+        else if(comando[0]=='C' && comando[1]=='E'){
+            consultarEstoque(&produto, &numeroProdutos);
+        }   
+        else if(comando[0]=='C' && comando[1]=='S'){
+            consultarSaldo(&saldoCaixa);
+        }
+        else if (comando[0] == 'F' && comando[1] == 'E') {
+            finalizarDia(produto, numeroProdutos, saldoCaixa);
+        }
     }
 
 
