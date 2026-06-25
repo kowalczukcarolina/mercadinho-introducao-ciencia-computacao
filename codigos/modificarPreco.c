85996af93e92f807dc65ce669177bec5449865fd
Rafael <rafaeltiosso@usp.br>
2026-06-25 17:57:05 -0300

    ATV#3

--- codigos/modificarPreco.c
+++ codigos/modificarPreco.c
@@ -2,11 +2,11 @@
 #include <stdlib.h>
 #include "produtos.h"
 
-void modificarPreco(Produtos **produto);
+void modificarPreco(Produtos *produto);
 
-void modificarPreco(Produtos **produto){
+void modificarPreco(Produtos *produto){
   int codigo;
   double precoNovo;
   scanf(" %d %lf", &codigo, &precoNovo);
-  (*produto)[codigo].preco=precoNovo;
+  produto[codigo].preco=precoNovo;
 }
