83c18c7398fd38a185f46c209c1b25e6fd3e5bae
VictorAdao <victoradao32@gmail.com>
2026-06-25 16:08:23 -0300

    ATV#8 - abrirCaixa Iniciado

---
+++ codigos/abrirCaixa.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "produtos.h"
+
+void abrirCaixa(int *ponteiroNumeroProdutos, Produtos **produto, double *ponteiroSaldo);
+
+void abrirCaixa(int *ponteiroNumeroProdutos, Produtos **produto, double *ponteiroSaldo){
+    FILE *arquivo;
+    arquivo = fopen("dados_mercado.txt", "r");
+
+    fscanf(arquivo, "%f", ponteiroSaldo);
+    fscanf(arquivo, "%d", ponteiroNumeroProdutos);
+
+    for(int i = 0; i<ponteiroNumeroProdutos; i++){
+        fscanf();
+    }
+
+
+}
